7bc8dc106432a9d7b5ad4539930b365598b86488
escotoj <j.escoto14@gmail.com>
2023-06-19 18:49:54 -0700

    components file has been created and applied to the app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Button } from "@mui/material";
+import { Typography } from "@mui/material";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
+import Project from "./components/Project";
 
 function App() {
   return (
     <div>
-      hello
-
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <Typography variant="h1" component="h2">
+        JE - Portfolio, Round 2
+      </Typography>
+      <Header/>
+      <Footer/>
+      <Navigation/>
+      <Project/>
     </div>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+  <div>Navigation</div>
+  )
+};
+
+export default Navbar

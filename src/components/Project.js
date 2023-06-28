85ab6bbfb83a424103f45ba6fc385726dc531140
escotoj <j.escoto14@gmail.com>
2023-06-27 21:07:03 -0700

    troubleshoot img paths in projetcs and deployment attempt 7

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,35 +7,35 @@ import { Typography } from '@mui/material';
 const Project = () => {
     const itemData = [
       {
-        img: '/public/Four-Leaf-CasinoApp.png',
+        img: '/React-Portfolio/Four-Leaf-CasinoApp.png',
         title: 'Full-Stack Casino Application',
         about: 'Collaboration using Express and MySQL',
       },
       {
-        img: './Techblog12.png',
+        img: 'React-Portfolio/Techblog12.png',
         title: 'Full-Stack Blog Application',
         about: 'Model-View-Controller Paradigm',
       }
       ,
 
       {
-        img: './weatherAppUpdate.png',
+        img: 'React-Portfolio/weatherAppUpdate.png',
         title: 'Weather Application',
         about: 'BootStrap and IPA requests',
       },
       {
-        img: './potterquizfinal.png',
+        img: 'React-Portfolio/potterquizfinal.png',
         title: 'HP Timed Quiz',
         about: 'Vanilla HTML, CSS and JavaScript',
       },
 
       {
-        img: './bootstrapPortfolio.png',
+        img: 'React-Portfolio/bootstrapPortfolio.png',
         title: 'Bootstrap Portfolio',
         about: 'CDN Bootstrap, Line Awesome and Animate On Scroll' 
       },
       {
-        img: './passwordGen2.png',
+        img: 'React-Portfolio/passwordGen2.png',
         title: 'Password Generator',
         about: 'Applied data structures and algorithms along with vanilla HTML, CSS and JavaScript',
       }

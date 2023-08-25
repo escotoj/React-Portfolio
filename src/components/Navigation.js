03064768082a636956c8920780c8922cb5171499
escotoj <j.escoto14@gmail.com>
2023-08-25 09:38:37 -0700

    add box to navbar to add margin

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,25 @@
 import { Whatshot } from "@material-ui/icons";
-import { Tab, Tabs } from "@mui/material";
+import { Tab, Tabs, Box } from "@mui/material";
 import { Link } from 'react-router-dom';
 import React from "react";
 
 const Navbar = () => {
   return (
-    
-<Tabs centered
-        sx={{
-          width: { xs: '100%', sm: '100%', lg: "100%" },
-          marginTop: "20px"
-          // height: { xs: 350, sm: 600, lg: 1000 },
-          
-        }}>
-  <Tab label="Portfolio" component={Link} to="/portfolio"/>
-  <Tab label="About Me" component={Link} to="/React-Portfolio/"/>
-  <Whatshot sx={{ display: { xs: "block", sm: "none" } }} />
-  <Tab label="Contact" component={Link} to="/contact"/>
-  <Tab label="Resume/CV" component={Link} to="/resume" />
-</Tabs>
+   <Box margin={2} >
+    <Tabs centered
+   sx={{
+     width: { xs: '100%', sm: '100%', lg: "100%" },
+     marginTop: "20px"
+     // height: { xs: 350, sm: 600, lg: 1000 },
+   
+   }}>
+<Tab label="Portfolio" component={Link} to="/portfolio"/>
+<Tab label="About Me" component={Link} to="/React-Portfolio/"/>
+<Whatshot sx={{ display: { xs: "block", sm: "none" } }} />
+<Tab label="Contact" component={Link} to="/contact"/>
+<Tab marginl label="Resume/CV" component={Link} to="/resume" />
+</Tabs></Box> 
+
     
   );
 };
